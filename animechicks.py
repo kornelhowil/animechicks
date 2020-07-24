c29396f76326eb552ea36e4b00ee168483f5187e
Kornel Howil <kornelhowil@MacBook-Pro.local>
2020-07-24 19:42:55 +0200

    added chick count

--- animechicks.py
+++ animechicks.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
-import random, os
-import pickle
+from flask import Flask, render_template, url_for
+import random, os, pickle
 
 app = Flask(__name__)
+
+
+@app.route('/chick1/<girl>')
+def click(girl):
+	with open('pickles/'+girl, 'rb') as dbfile:
+		chick = pickle.load(dbfile)
+		chick['count'] += 1
+
+	with open('pickles/'+girl, 'wb') as dbfile:
+		pickle.dump(chick, dbfile)   
+
+	return entry_page()
+		
 
 @app.route('/')
 def entry_page():
@@ -17,17 +29,20 @@ def entry_page():
 			break
 
 	with open('pickles/'+girl1, 'rb') as dbfile:
-		print(girl1)
 		chick1 = pickle.load(dbfile)
+		print(chick1['count'])
 
 	with open('pickles/'+girl2, 'rb') as dbfile:
-		print(girl2)
 		chick2 = pickle.load(dbfile)
-
+		print(chick2['count'])
 
 	return render_template('index.html', the_title='Anime Chicks',
-										 the_path1=chick1['image'],
-										 the_path2=chick2['image'])
+										 the_chick1=chick1,
+										 the_chick2=chick2,
+										 the_girl1=girl1,
+										 the_girl2=girl2)
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
