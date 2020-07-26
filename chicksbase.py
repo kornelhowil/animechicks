545864d695229f9810e8f666f2c351a6f34bf3a1
Kornel Howil <kornelhowil@gmail.com>
2020-07-26 17:24:32 +0200

    Added more chicks

--- chicksbase.py
+++ chicksbase.py
@@ -71,6 +71,126 @@ ZeroTwo = {	'name': 'Zero Two',
 			'image': '/static/images/14.jpg',
 			'count': 0}
 
+CC = {		'name': 'C.C',
+			'url': 'https://myanimelist.net/character/1111/CC',
+			'image': '/static/images/15.jpg',
+			'count': 0}
+
+Izumi = {	'name': 'Konata Izumi',
+			'url': 'https://myanimelist.net/character/2169/Konata_Izumi',
+			'image': '/static/images/16.jpg',
+			'count': 0}
+  
+Rikka = {	'name': 'Rikka Takanashi',
+			'url': 'https://myanimelist.net/character/65865/Rikka_Takanashi',
+			'image': '/static/images/17.jpg',
+			'count': 0}
+
+Kaori = {	'name': 'Kaori Miyazono',
+			'url': 'https://myanimelist.net/character/69411/Kaori_Miyazono',
+			'image': '/static/images/18.jpg',
+			'count': 0}
+
+Asuka = {	'name': 'Asuka Langley Souryuu',
+			'url': 'https://myanimelist.net/character/94/Asuka_Langley_Souryuu',
+			'image': '/static/images/19.jpg',
+			'count': 0}
+
+Emilia = {	'name': 'Emilia',
+			'url': 'https://myanimelist.net/character/118737/Emilia',
+			'image': '/static/images/20.jpg',
+			'count': 0}
+ 
+Erza = {	'name': 'Erza Scarlet',
+			'url': 'https://myanimelist.net/character/5189/Erza_Scarlet',
+			'image': '/static/images/21.jpg',
+			'count': 0}
+
+Haruhi = {	'name': 'Haruhi Suzumiya',
+			'url': 'https://myanimelist.net/character/251/Haruhi_Suzumiya',
+			'image': '/static/images/22.jpg',
+			'count': 0}
+
+Revy = {	'name': 'Revy',
+			'url': 'https://myanimelist.net/character/458/Revy',
+			'image': '/static/images/23.jpg',
+			'count': 0}
+
+Kurumi = {	'name': 'Kurumi Tokisaki',
+			'url': 'https://myanimelist.net/character/70069/Kurumi_Tokisaki',
+			'image': '/static/images/24.jpg',
+			'count': 0}
+
+Shiina = {	'name': 'Mashiro Shiina',
+			'url': 'https://myanimelist.net/character/61371/Mashiro_Shiina',
+			'image': '/static/images/25.jpg',
+			'count': 0}
+
+Mai = {		'name': 'Mai Sakurajima',
+			'url': 'https://myanimelist.net/character/118739/Mai_Sakurajima',
+			'image': '/static/images/26.jpg',
+			'count': 0}
+
+Rin = {		'name': 'Rin Toosaka',
+			'url': 'https://myanimelist.net/character/498/Rin_Toosaka',
+			'image': '/static/images/27.jpg',
+			'count': 0}
+
+Mikoto = {	'name': 'Mikoto Misaka',
+			'url': 'https://myanimelist.net/character/13701/Mikoto_Misaka',
+			'image': '/static/images/28.jpg',
+			'count': 0}
+
+Rias = {	'name': 'Rias Gremory',
+			'url': 'https://myanimelist.net/character/50389/Rias_Gremory',
+			'image': '/static/images/29.jpg',
+			'count': 0}
+
+Violet = {	'name': 'Violet Evergarden',
+			'url': 'https://myanimelist.net/character/141354/Violet_Evergarden',
+			'image': '/static/images/30.jpg',
+			'count': 0}
+
+Shiro = {	'name': 'Shiro',
+			'url': 'https://myanimelist.net/character/82525/Shiro',
+			'image': '/static/images/31.jpg',
+			'count': 0}
+
+Motoko = {	'name': 'Motoko Kusanagi',
+			'url': 'https://myanimelist.net/character/1795/Motoko_Kusanagi',
+			'image': '/static/images/32.jpg',
+			'count': 0}
+
+Lucy = {	'name': 'Lucy',
+			'url': 'https://myanimelist.net/character/738/Lucy',
+			'image': '/static/images/33.jpg',
+			'count': 0}
+
+Nishimija = {'name': 'Shouko Nishimiya',
+			'url': 'https://myanimelist.net/character/80243/Shouko_Nishimiya',
+			'image': '/static/images/34.jpg',
+			'count': 0}
+
+Chitoge = {	'name': 'Chitoge Kirisak',
+			'url': 'https://myanimelist.net/character/48391/Chitoge_Kirisaki',
+			'image': '/static/images/35.jpg',
+			'count': 0}
+
+Kanade = {'	name': 'Kanade Tachibana',
+			'url': 'https://myanimelist.net/character/22369/Kanade_Tachibana',
+			'image': '/static/images/35.jpg',
+			'count': 0}
+
+Matoi = {'	name': 'Ryuuko Matoi ',
+			'url': 'https://myanimelist.net/character/83797/Ryuuko_Matoi',
+			'image': '/static/images/36.jpg',
+			'count': 0}
+
+Ayanami = {'name': 'Rei Ayanami',
+			'url': 'https://myanimelist.net/character/86/Rei_Ayanami',
+			'image': '/static/images/37.jpg',
+			'count': 0}
+
 with open('pickles/makise.pickle', 'ab') as dbfile:
 	pickle.dump(Makise, dbfile)  
 	
@@ -111,4 +231,76 @@ with open('pickles/homura.pickle', 'ab') as dbfile:
 	pickle.dump(Homura, dbfile) 
 
 with open('pickles/zerotwo.pickle', 'ab') as dbfile:
-	pickle.dump(ZeroTwo, dbfile)   
+	pickle.dump(ZeroTwo, dbfile)  
+
+with open('pickles/cc.pickle', 'ab') as dbfile:
+	pickle.dump(CC, dbfile)  
+
+with open('pickles/izumi.pickle', 'ab') as dbfile:
+	pickle.dump(Izumi, dbfile)    
+
+with open('pickles/rikka.pickle', 'ab') as dbfile:
+	pickle.dump(Rikka, dbfile)
+
+with open('pickles/kaori.pickle', 'ab') as dbfile:
+	pickle.dump(Kaori, dbfile)    
+
+with open('pickles/asuka.pickle', 'ab') as dbfile:
+	pickle.dump(Asuka, dbfile)   
+
+with open('pickles/emilia.pickle', 'ab') as dbfile:
+	pickle.dump(Emilia, dbfile)   
+
+with open('pickles/erza.pickle', 'ab') as dbfile:
+	pickle.dump(Erza, dbfile)
+
+with open('pickles/haruhi.pickle', 'ab') as dbfile:
+	pickle.dump(Haruhi, dbfile)    
+
+with open('pickles/revy.pickle', 'ab') as dbfile:
+	pickle.dump(Revy, dbfile)   
+
+with open('pickles/kurumi.pickle', 'ab') as dbfile:
+	pickle.dump(Kurumi, dbfile) 
+
+with open('pickles/shiina.pickle', 'ab') as dbfile:
+	pickle.dump(Shiina, dbfile) 
+
+with open('pickles/mai.pickle', 'ab') as dbfile:
+	pickle.dump(Mai, dbfile) 
+
+with open('pickles/rin.pickle', 'ab') as dbfile:
+	pickle.dump(Rin, dbfile) 
+
+with open('pickles/mikoto.pickle', 'ab') as dbfile:
+	pickle.dump(Mikoto, dbfile)
+
+with open('pickles/rias.pickle', 'ab') as dbfile:
+	pickle.dump(Rias, dbfile)  
+
+with open('pickles/violet.pickle', 'ab') as dbfile:
+	pickle.dump(Violet, dbfile)  
+
+with open('pickles/shiro.pickle', 'ab') as dbfile:
+	pickle.dump(Shiro, dbfile)  
+
+with open('pickles/motoko.pickle', 'ab') as dbfile:
+	pickle.dump(Motoko, dbfile) 
+
+with open('pickles/lucy.pickle', 'ab') as dbfile:
+	pickle.dump(Lucy, dbfile) 
+
+with open('pickles/nishimija.pickle', 'ab') as dbfile:
+	pickle.dump(Nishimija, dbfile)
+
+with open('pickles/chitoge.pickle', 'ab') as dbfile:
+	pickle.dump(Chitoge, dbfile)
+
+with open('pickles/kanade.pickle', 'ab') as dbfile:
+	pickle.dump(Kanade, dbfile)
+
+with open('pickles/matoi.pickle', 'ab') as dbfile:
+	pickle.dump(Matoi, dbfile)
+
+with open('pickles/ayanami.pickle', 'ab') as dbfile:
+	pickle.dump(Ayanami, dbfile)
